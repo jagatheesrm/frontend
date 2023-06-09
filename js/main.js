0da528f776a12a1ce5656817e8eb272ab36092d8
Jagathees waran <61326194+jagatheesrm@users.noreply.github.com>
2023-06-09 23:30:25 +0530

    09.06.2023_chalange

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-function login() {
-    var email = document.getElementById("login-email").value;
-    var password = document.getElementById("login-password").value;
+// function login() {
+//     var email = document.getElementById("login-email").value;
+//     var password = document.getElementById("login-password").value;
 
-    if (email === "" || password === "") {
-      document.getElementById("login-message").innerHTML = "Please enter both email and password.";
-    } else {
-      // Perform login logic here
-      document.getElementById("login-message").innerHTML = "Logging in...";
-    }
-  }
+//     if (email === "" || password === "") {
+//       document.getElementById("login-message").innerHTML = "Please enter both email and password.";
+//     } else {
+//       // Perform login logic here
+//       document.getElementById("login-message").innerHTML = "Logging in...";
+//     }
+//   }
 
-  function register() {
-    var email = document.getElementById("register-email").value;
-    var password = document.getElementById("register-password").value;
+//   function register() {
+//     var email = document.getElementById("register-email").value;
+//     var password = document.getElementById("register-password").value;
 
-    if (email === "" || password === "") {
-      document.getElementById("register-message").innerHTML = "Please enter both email and password.";
-    } else {
-      // Perform registration logic here
-      document.getElementById("register-message").innerHTML = "Registering...";
-    }
-  }
+//     if (email === "" || password === "") {
+//       document.getElementById("register-message").innerHTML = "Please enter both email and password.";
+//     } else {
+//       // Perform registration logic here
+//       document.getElementById("register-message").innerHTML = "Registering...";
+//     }
+//   }
